e3d6ebc101df660c3ef917dc566476f97d1be7fb
Lucascho <labluce@gmail.com>
2019-03-07 00:57:01 +0800

    Update main.go

--- main.go
+++ main.go
@@ -52,15 +52,14 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 				//if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.ID+":"+message.Text+" OK!")).Do(); err != nil {
 				//	log.Print(err)
 				//}
+				
 				inText := strings.ToLower(message.Text)
 				if strings.Contains(inText, "狗") || strings.Contains(inText, "dog") {
 					out := "汪"
-					
-				}
-				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(out)).Do(); err != nil {
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(out)).Do(); err != nil {
 					log.Print(err)
+					}
 				}
-				
 			}
 		}
 	}
