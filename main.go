81394b7b86135506c528ca1bb2fdc8b3ca7e18df
Lucascho <labluce@gmail.com>
2019-03-07 01:51:50 +0800

    Update main.go

--- main.go
+++ main.go
@@ -54,7 +54,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 				//	log.Print(err)
 				//}
 				log.Println(message.Text)
-				
+				var out = ""
 				inText := strings.ToLower(message.Text)
 				if strings.Contains(inText, "狗") || strings.Contains(inText, "dog") {
 					out = "汪"
