b41a67ba32b87f201c74283cc6fd333a28bfd9fb
Lucascho <labluce@gmail.com>
2019-03-07 01:12:32 +0800

    Update main.go

--- main.go
+++ main.go
@@ -49,17 +49,17 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 		if event.Type == linebot.EventTypeMessage {
 			switch message := event.Message.(type) {
 			case *linebot.TextMessage:
-				//if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.ID+":"+message.Text+" OK!")).Do(); err != nil {
-				//	log.Print(err)
-				//}
+				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.ID+":"+message.Text+" OK!")).Do(); err != nil {
+					log.Print(err)
+				}
 				//out := "喵"
 				//inText := strings.ToLower(message.Text)
 				//if strings.Contains(inText, "狗") || strings.Contains(inText, "dog") {
 				//	out := "汪"
 				//}
-				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("測試成功")).Do(); err != nil {
-					log.Print(err)
-				}
+				//if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("測試成功")).Do(); err != nil {
+				//	log.Print(err)
+				//}
 			}
 		}
 	}
