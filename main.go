e13f255b190c09e3b7887b52103bffad5e618cd0
Lucascho <labluce@gmail.com>
2019-03-11 20:42:40 +0800

    Update main.go

--- main.go
+++ main.go
@@ -144,7 +144,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 				//if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewImageMessage("https://www.google.com.tw/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png","https://www.google.com.tw/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png")).Do(); err != nil {
 				//	log.Print(err)
 				//}
-				if out != nil {
+				if out != "" {
 				log.Println(message.Text)
 					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(out)).Do(); err != nil {
 						log.Print(err)
